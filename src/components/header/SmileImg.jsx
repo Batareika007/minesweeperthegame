0d737cb0f5ef97e1e98581fd870e33503953a84e
Alexei <50496651+Batareika007@users.noreply.github.com>
2023-03-04 16:32:58 +0300

    testing

--- src/components/header/SmileImg.jsx
+++ src/components/header/SmileImg.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function SmileImg({ smileMood }) {
+function SmileImg({ changeImg }) {
+	const [currentImage, setImage] = useState('sprite_smile_regular')
+	
 	return (
 		<div className='header__part'>
-			<div className={`sprite sprite_smile_size ${smileMood}`}></div>
+			<div
+				className={`sprite sprite_smile_size ${currentImage}`}
+				onClick={() => {setImage(changeImg)	}}></div>
 		</div>
 	)
 }
